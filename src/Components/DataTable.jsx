f495a2d5b1029af03587575b116cdf7fd3d13c26
Aadi Jain <aadijaintikamgarh@gmail.com>
2025-02-18 11:59:56 +0530

    add new entry and delete button

--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -44,54 +44,69 @@ function DataTable() {
   return (
     <div className="flex flex-col gap-3 p-4 px-5 w-full h-dvh bg-zinc-950">
       <MantineProvider>
-        <div className="flex justify-end gap-4">
-          <Input
-            placeholder="Customer Name"
-            styles={{
-              input: {
-                width: "20vw",
-                background: "transparent",
-                color: "white",
-              },
-            }}
-            value={customer}
-            onChange={(e) => setCustomer(e.target.value)}
-          />
-          <Input
-            placeholder="Department"
-            styles={{
-              input: {
-                width: "20vw",
-                background: "transparent",
-                color: "white",
-              },
-            }}
-            value={department}
-            onChange={(e) => setDepartment(e.target.value)}
-          />
+        <div className="flex flex-col items-end gap-2">
+          <div className="flex justify-end gap-4">
+            <Input
+              placeholder="Customer Name"
+              styles={{
+                input: {
+                  width: "20vw",
+                  background: "transparent",
+                  color: "white",
+                },
+              }}
+              value={customer}
+              onChange={(e) => setCustomer(e.target.value)}
+            />
+            <Input
+              placeholder="Department"
+              styles={{
+                input: {
+                  width: "20vw",
+                  background: "transparent",
+                  color: "white",
+                },
+              }}
+              value={department}
+              onChange={(e) => setDepartment(e.target.value)}
+            />
 
-          <Button
-            variant="filled"
-            style={{ width: "9vw", minWidth: "80px" }}
-            onClick={() => {
-              setfilteredData(
-                data.filter((ele) => {
-                  if (customer === "" && department === "") return true;
-                  if (customer === "" && department !== "")
-                    return ele.department.toLowerCase() === department.toLowerCase();
-                  if (customer !== "" && department === "")
-                    return ele.name.toLowerCase() === customer.toLowerCase();
-                  if (customer !== "" && department !== "")
-                    return (
-                      ele.name.toLowerCase() === customer.toLowerCase() && ele.department.toLowerCase() === department.toLowerCase()
-                    );
-                })
-              );
-              console.log(customer, department);
-            }}
-          >
-            Search
-          </Button>
+            <Button
+              variant="filled"
+              style={{ width: "9vw", minWidth: "80px" }}
+              onClick={() => {
+                setfilteredData(
+                  data.filter((ele) => {
+                    if (customer === "" && department === "") return true;
+                    if (customer === "" && department !== "")
+                      return (
+                        ele.department.toLowerCase() ===
+                        department.toLowerCase()
+                      );
+                    if (customer !== "" && department === "")
+                      return ele.name.toLowerCase() === customer.toLowerCase();
+                    if (customer !== "" && department !== "")
+                      return (
+                        ele.name.toLowerCase() === customer.toLowerCase() &&
+                        ele.department.toLowerCase() ===
+                          department.toLowerCase()
+                      );
+                  })
+                );
+                console.log(customer, department);
+              }}
+            >
+              Search
+            </Button>
+          </div>
+          <div className="flex gap-4">
+            <Button variant="filled" style={{ width: "9vw", minWidth: "80px" }}>
+              New Entry
+            </Button>
+            <Button variant="filled" style={{ width: "9vw", minWidth: "80px", backgroundColor: "red" }}>
+              Delete
+            </Button>
+          </div>
         </div>
         <div className="border-1 border-zinc-500 overflow-y-scroll [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-zinc-800 [&::-webkit-scrollbar]:w-2">
           <Table
